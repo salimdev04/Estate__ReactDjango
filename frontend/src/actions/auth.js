0c9cd9da29d723dc21b9152c2be93ff645a20b0b
saimdev <60602690+salimdev04@users.noreply.github.com>
2021-06-25 10:21:12 +0100

    Estate fullStack project with react django

---
+++ frontend/src/actions/auth.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import { setAlert } from './alert';
+import { LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT, SIGNUP_FAIL, SIGNUP_SUCCESS } from './types';
+
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    const body = JSON.stringify({ email, password })
+
+    try {
+        const res = await axios.post('http://127.0.0.1:8000/api/token/', body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        })
+
+        dispatch(setAlert('Authentication successfuly', 'success'));
+    } catch (err) {
+        dispatch({
+            type: LOGIN_FAIL
+        });
+        dispatch(setAlert('Error Authenticating', 'error'))
+    }
+}
+
+export const signup = (name, email, password, password2) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    const body = JSON.stringify({ name, email, password, password2 })
+
+    try {
+        const res = await axios.post('http://127.0.0.1:8000/api/accounts/signup', body, config);
+
+        dispatch({
+            type: SIGNUP_SUCCESS,
+            payload: res.data
+        })
+
+        dispatch(login(email, password));
+    } catch (err) {
+        dispatch({
+            type: SIGNUP_FAIL
+        });
+        dispatch(setAlert('Error Authenticating', 'error'))
+    }
+}
+
+export const logout = () => dispatch => {
+    dispatch(setAlert('logout successful', 'success'))
+    dispatch({ type: LOGOUT })
+}
